f8ecc06d9e0d9e3f87162e8cb6595975b0f4da75
Douglas Alves <92764334+DougA190@users.noreply.github.com>
2023-05-24 16:03:47 -0300

    Delete 4Tech directory

--- 4Tech/4Tech/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#sair').click(function() {
-        alert('Sua sessão será encerrada!');
-        window.location.href = 'sairCliente.php';
-    });
-});
-
--- 4Tech/README.md
+++
@@ -1,8 +0,0 @@
-# 4Tech
-Projeto Integrador - 4º Semestre
-
-## Ícones
-- :package: Nova funcionalidade
-- :up: Atualização
-- :bug:  Correção de bug
-- :checkered_flag: Release
